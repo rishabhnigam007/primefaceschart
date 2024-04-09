c62a1b517503376548906da88520e7520ca6ec57
rishabh nigam <rishabhakans@gmail.com>
2024-04-10 01:25:14 +0530

    Prime faces chart is practicing

---
+++ src/main/java/com/chart/primefaceschart/DonutChart.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chart.primefaceschart;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.donut.DonutChartDataSet;
+import org.primefaces.model.charts.donut.DonutChartModel;
+import org.primefaces.model.charts.donut.DonutChartOptions;
+
+/**
+ *
+ * @author Rishabh
+ */
+@ManagedBean(name = "donutChart")
+@RequestScoped
+public class DonutChart implements Serializable {
+
+    private DonutChartModel donutModel;
+
+    @PostConstruct
+    public void init() {
+        createDonutModel();
+    }
+
+    public DonutChartModel getDonutModel() {
+        return donutModel;
+    }
+
+    public void setDonutModel(DonutChartModel donutModel) {
+        this.donutModel = donutModel;
+    }
+
+    public void createDonutModel() {
+        donutModel = new DonutChartModel();
+        ChartData data = new ChartData();
+        DonutChartOptions options = new DonutChartOptions();
+        options.setMaintainAspectRatio(false);
+        donutModel.setOptions(options);
+
+        DonutChartDataSet dataSet = new DonutChartDataSet();
+        List<Number> values = new ArrayList<>();
+        values.add(300);
+        values.add(50);
+        values.add(100);
+        dataSet.setData(values);
+
+        List<String> bgColors = new ArrayList<>();
+        bgColors.add("rgb(255, 99, 132)");
+        bgColors.add("rgb(54, 162, 235)");
+        bgColors.add("rgb(255, 205, 86)");
+        dataSet.setBackgroundColor(bgColors);
+
+        data.addChartDataSet(dataSet);
+        List<String> labels = new ArrayList<>();
+        labels.add("Red");
+        labels.add("Blue");
+        labels.add("Yellow");
+        data.setLabels(labels);
+
+        donutModel.setData(data);
+    }
+
+}
