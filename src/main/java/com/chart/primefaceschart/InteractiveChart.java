eb45e66e37d78288d1056662302c2ed032a230d5
rishabh nigam <rishabhakans@gmail.com>
2024-04-16 18:39:55 +0530

    export chart only left and only export dialog not coming

---
+++ src/main/java/com/chart/primefaceschart/InteractiveChart.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chart.primefaceschart;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.ItemSelectEvent;
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.axes.cartesian.CartesianScales;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearTicks;
+import org.primefaces.model.charts.bar.BarChartDataSet;
+import org.primefaces.model.charts.bar.BarChartModel;
+import org.primefaces.model.charts.bar.BarChartOptions;
+import org.primefaces.model.charts.optionconfig.title.Title;
+
+/**
+ *
+ * @author Rishabh
+ */
+@ManagedBean(name = "interactiveChart")
+@RequestScoped
+public class InteractiveChart implements Serializable {
+
+    private BarChartModel barChartModel;
+
+    public BarChartModel getBarChartModel() {
+        return barChartModel;
+    }
+
+    public void setBarChartModel(BarChartModel barChartModel) {
+        this.barChartModel = barChartModel;
+    }
+
+    @PostConstruct
+    public void init() {
+        createInterativeModel();
+    }
+
+    public void createInterativeModel() {
+
+        barChartModel = new BarChartModel();
+        ChartData data = new ChartData();
+
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        barDataSet.setLabel("My First Dataset");
+        barDataSet.setBackgroundColor("rgba(255, 99, 132, 0.2)");
+        barDataSet.setBorderColor("rgb(255, 99, 132)");
+        barDataSet.setBorderWidth(1);
+        List<Number> values = new ArrayList<>();
+        values.add(65);
+        values.add(59);
+        values.add(80);
+        values.add(81);
+        barDataSet.setData(values);
+
+        BarChartDataSet barDataSet2 = new BarChartDataSet();
+        barDataSet2.setLabel("My Second Dataset");
+        barDataSet2.setBackgroundColor("rgba(75, 192, 192, 0.2)");
+        barDataSet2.setBorderColor("rgb(75, 192, 192)");
+        barDataSet2.setBorderWidth(1);
+        List<Number> values2 = new ArrayList<>();
+        values2.add(85);
+        values2.add(69);
+        values2.add(20);
+        values2.add(51);
+        barDataSet2.setData(values2);
+
+        data.addChartDataSet(barDataSet);
+        data.addChartDataSet(barDataSet2);
+
+        List<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        data.setLabels(labels);
+        barChartModel.setData(data);
+
+        //Options
+        BarChartOptions options = new BarChartOptions();
+        options.setMaintainAspectRatio(false);
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearAxes = new CartesianLinearAxes();
+        linearAxes.setOffset(true);
+        linearAxes.setBeginAtZero(true);
+        CartesianLinearTicks ticks = new CartesianLinearTicks();
+        linearAxes.setTicks(ticks);
+        cScales.addYAxesData(linearAxes);
+        options.setScales(cScales);
+
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Bar Chart");
+        options.setTitle(title);
+
+        barChartModel.setOptions(options);
+    }
+
+    // for ajax messag and growl
+    public void itemSelect(ItemSelectEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
+                "Item Index: " + event.getItemIndex() + ", DataSet Index:" + event.getDataSetIndex());
+
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+}
